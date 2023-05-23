88efc9c6a161255bda6b65a2b46dd4c21e2a714f
Batnasan <b20fa1703@ufe.edu.mn>
2023-05-23 22:51:28 +0800

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
   // BACKEND_URL: "https://agm.mn",
   // BACKEND_URL: "http://agm.bdsec.mn",
-  BACKEND_URL: "http://localhost:4001",
+  BACKEND_URL: "http://192.168.111.128:4004",
   BACKEND_URL2: "http://192.168.111.128:4003",
   APP_VERSION: "v1",
 };
